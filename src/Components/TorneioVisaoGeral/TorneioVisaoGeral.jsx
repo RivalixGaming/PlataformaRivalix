212d272359b5156ad918e95ab37d7e93ece87668
Renan Paulino <dev.renanpaulino@gmail.com>
2025-06-25 21:59:00 -0300

    Atualização pag torneio

--- src/Components/TorneioVisaoGeral/TorneioVisaoGeral.jsx
+++ src/Components/TorneioVisaoGeral/TorneioVisaoGeral.jsx
@@ -2,64 +2,39 @@ import torneios from "../../data/torneios";
 import style from "./TorneioVisaoGeral.module.css"
 
 
-export default function TorneioVisaoGeral({torneio}) {
-
-    const torn = torneios[torneio-1]
-
-    return(
-        <>
-                
-
-            <div className={style.container_todo_detalhe_torneio}>
-                <div className={style.container_detalhes_torneio}>
-                    <h2 style={{paddingBottom:"30px", fontSize:"30px", width:"100%"}}>Detalhes</h2>
-                    <div>
-                        <h4>jogo</h4>
-                        <p>{torn.jogo}</p>
-                    </div>
-                    <div>
-                        <h4>Equipe</h4>
-                        <p>{torn.modalidade}</p>
-                    </div>
-                    <div>
-                        <h4>Formato</h4>
-                        <p>{torn.formato}</p>
-                    </div>
-                    <div>
-                        <h4>Tipo</h4>
-                        <p>{torn.tipo}</p>
-                    </div>
-                    <div>
-                        <h4>Inicio</h4>
-                        <p>{torn.data}</p>
-                        <p>{torn.hora}</p>
-                    </div>
-                    <div>
-                        <h4>Prêmio Total</h4>
-                        <p>R$ {torn.premioTotal},00</p>
-                    </div>
-                    <div>
-                        <h4>Entrada</h4>
-                        <p>R$ {torn.entradaValor},00</p>
-                    </div>
-                    <div>
-                        <h4>Endereço</h4>
-                        <p>{torn.endereco}</p>
-                    </div>
-                </div>
-                <div className={style.container_participantes}>
-                    <div className={style.container_informacoes_particpantes}> 
-                        <h2>Participantes</h2>
-                        <p><i class="ri-team-fill"></i> {torn.totalVagas - torn.vagasRestantes}/{torn.totalVagas}</p>
-                        <div className={style.linha_participantes}></div>
-                        <h2>Prêmio</h2>
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <p>1º Lugar</p>
-                            <p>R$ {torn.premioTotal},00</p>
-                        </div>
-                    </div>
-                </div>
+export default function TorneioVisaoGeral({ torneio }) {
+  return (
+    <div className={style.container_todo_detalhe_torneio}>
+        <div className={style.container_detalhes_torneio}>
+            <h2 className={style.titulo}>Detalhes</h2>
+            <div className={style.detGrid}>
+                <div><h4>Jogo</h4>           <p>{torneio.jogo}</p></div>
+                <div><h4>Equipe</h4>         <p>{torneio.modalidade}</p></div>
+                <div><h4>Formato</h4>        <p>{torneio.formato}</p></div>
+                <div><h4>Tipo</h4>           <p>{torneio.tipo}</p></div>
+                <div><h4>Início</h4>         <p>{torneio.data} {torneio.hora}</p></div>
+                <div><h4>Prêmio Total</h4>   <p>R$ {torneio.premioTotal}</p></div>
+                <div><h4>Entrada</h4>        <p>R$ {torneio.entradaValor}</p></div>
+                <div><h4>Endereço</h4>       <p>{torneio.endereco}</p></div>
             </div>
-        </>
-    )
+      </div>
+
+      {/* bloco de participantes / prêmio */}
+      <div className={style.container_participantes}>
+        <div className={style.container_informacoes_particpantes}>
+          <h2>Participantes</h2>
+          <p>
+            <i className="ri-team-fill"></i>{" "}
+            {torneio.totalVagas - torneio.vagasRestantes}/{torneio.totalVagas}
+          </p>
+          <div className={style.linha_participantes}></div>
+          <h2>Prêmio</h2>
+          <div className={style.premio_lugar}>
+            <p>1º Lugar</p>
+            <p>R$ {torneio.premioTotal}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
