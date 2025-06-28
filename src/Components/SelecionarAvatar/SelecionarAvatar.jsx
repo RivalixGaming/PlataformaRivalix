136628eddbb51dcbc7e9947cab8ca6125e1764c2
Renan Paulino <dev.renanpaulino@gmail.com>
2025-06-27 22:56:32 -0300

    Finalizado os avatares e reajuste na responsividade do ranking

--- src/Components/SelecionarAvatar/SelecionarAvatar.jsx
+++ src/Components/SelecionarAvatar/SelecionarAvatar.jsx
@@ -6,6 +6,9 @@ import avatar2 from "../../assets/avatars/avatar2.png";
 import avatar3 from "../../assets/avatars/avatar3.png"; 
 import avatar4 from "../../assets/avatars/avatar4.png";
 import avatar5 from "../../assets/avatars/avatar5.png";
+import avatar6 from "../../assets/avatars/avatar6.png";
+import avatar7 from "../../assets/avatars/avatar7.png";
+import avatar8 from "../../assets/avatars/avatar8.png";
 
 const avataresPadrao = [
     { nome: "Hero", src: avatar1 },
@@ -13,6 +16,9 @@ const avataresPadrao = [
     { nome: "TheCreator", src: avatar3 },
     { nome: "Falko", src: avatar4 },
     { nome: "Capi", src: avatar5 },
+    { nome: "SoarZ", src: avatar6 },
+    { nome: "Ainz", src: avatar7 },
+    { nome: "Wolfx", src: avatar8 },
 ];
 
 export default function SelecionarAvatar({ onClose }) {
